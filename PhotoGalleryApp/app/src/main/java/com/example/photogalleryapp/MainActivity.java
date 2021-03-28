ca2a5b25cb0ba077eb155e357ae85d8b5058207f
BillalR <billalrahimi@hotmail.com>
2021-03-28 12:15:04 -0700

    Weather Update

--- PhotoGalleryApp/app/src/main/java/com/example/photogalleryapp/MainActivity.java
+++ PhotoGalleryApp/app/src/main/java/com/example/photogalleryapp/MainActivity.java
@@ -12,16 +12,25 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 //import android.content.pm.PackageManager;
 //import android.graphics.Bitmap;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 //import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Debug;
 import android.os.Environment;
 //import android.os.StrictMode;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,10 +41,19 @@ import android.widget.TextView;
 //import com.google.android.gms.location.LocationServices;
 //import com.google.android.gms.tasks.Task;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 //import java.io.FileOutputStream;
 import java.io.IOError;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DateFormat;
 //import java.text.DecimalFormat;
 import java.text.ParseException;
@@ -43,9 +61,11 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity  implements LocationListener {
     public LocationManager locationManager;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int SEARCH_ACTIVITY_REQUEST_CODE = 2;
@@ -55,12 +75,99 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     public static final String EXTRA_MESSAGE = "com.example.PhotoGalleryApp.MESSAGE";
     public Button shareButton;
     public ImageView ima;
+    TextView weatherDisplay;
 
 
+    class Weather extends AsyncTask<String, Void, String> { //First stream means URL, void is nothing, third string means return type will be String
+
+        public void searchWeather(String location){
+            String content;
+            Weather weather = new Weather();
+            weatherDisplay = findViewById(R.id.weather);
+
+            try {
+                content = weather.execute("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=84b00d028b446f75b2dfcf13744a9964").get();
+                //First check data is retreived successfully
+                Log.i("content", content);
+
+                //JSON
+                JSONObject jsonObject = new JSONObject(content);
+                String weatherData = jsonObject.getString("weather");
+                String mainTemperature = jsonObject.getString("main"); //Temperature
+
+                Log.i("weatherData", weatherData);
+                //weather data is in array
+                JSONArray array = new JSONArray(weatherData);
+
+                String main = "";
+                String description = "";
+                String temperatureKelvin = "";
+
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject weatherPart = array.getJSONObject(i);
+                    main = weatherPart.getString("main");
+                    description = weatherPart.getString("description");
+                }
+
+                JSONObject mainPart = new JSONObject(mainTemperature);
+                temperatureKelvin = mainPart.getString("temp");
+                Double temp = new Double(temperatureKelvin);
+                temp = temp - 273.15;
+                String temperatureCelcius = String.format("%.2f", temp);
+
+
+                Log.i("main", main);
+                Log.i("description", description);
+                weatherDisplay.setText("Main:" + main + "\nDescription: " + description + "\nTemperature: " + temperatureCelcius + "Celcius");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+
+        }
+        @Override
+        protected String doInBackground(String... address) {
+            //String... means multiple address can be sent, it acts as an array
+            try {
+                URL url = new URL(address[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream is = connection.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+
+                int data = isr.read();
+                String content = "";
+                char ch;
+                while (data != -1){
+                    ch = (char) data;
+                    content = content + ch;
+                    data = isr.read();
+
+                }
+                return content;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        Debug.startMethodTracing("onCreate");
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        Weather displayWeather = new Weather();
+        displayWeather.searchWeather("London");
+
         shareButton = (Button) findViewById((R.id.share));
         ima = (ImageView) findViewById(R.id.imageView);
 
@@ -78,24 +185,34 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 e.printStackTrace();
             }
         }
-
         shareButton.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View v){
                     image();
-
             }
         });
+//        Debug.stopMethodTracing();
     }
+
+//    @Override
+//    protected void onStop() {
+//        Debug.stopMethodTracing();
+//        super.onStop();
+//    }
+//
+
+
+
 
     /** Called when the user tap    1s the Send button */
     public void search(View view) {
+
         // Do something in response to button
         Intent intent = new Intent(this, SearchActivity.class);
         startActivityForResult(intent, SEARCH_ACTIVITY_REQUEST_CODE);
     }
 
     public void takePhoto(View v) {
+        Debug.startMethodTracing("takePhoto");
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         //if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
         File photoFile = null;
@@ -112,6 +229,23 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         }
         //}
     }
+
+
+//
+//    protected void onDraw(Canvas canvas){
+//        canvas.drawColor(Color.WHITE);
+//        Paint p = new Paint();
+//        float y = 10;
+//        SparseArray<String> sparseArrayapp
+//build.gradle
+//gradle
+//gradle.properties
+//gradlew
+//gradlew.bat
+//PhotoGalleryApp.plantuml
+//settings.gradle = new SparseArray<>();
+//    }
+
 
     private ArrayList<String> findPhotos(Date startTimestamp, Date endTimestamp, String keywords,String latitude, String longitude) {
         File file = new File(Environment.getExternalStorageDirectory()
@@ -231,6 +365,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                         e.printStackTrace();
                     }
                 }
+
         }
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             getLocation();
@@ -243,6 +378,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             } catch (ParseException e) {
                 e.printStackTrace();
             }
+            Debug.stopMethodTracing();
         }
     }
     private void image(){
